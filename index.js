32a3feb723c779a1271bbcae16f0e59da16903e1
sivasubramanian11 <Sivaifs4@gmail.com>
2022-08-25 19:07:02 +0530

    Logic building

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-html
+const billamount = document.querySelector(".bill-amount");
+const cashgiven = document.querySelector(".cash-given");
+const checkbutton = document.querySelector(".check-button");
+const message = document.querySelector(".error-message");
+const noOfNotes = document.querySelector(".no-Of-Notes");
+
+const availableNotes = [2000, 500, 100, 20, 10, 1];
+
+checkbutton.addEventListener("click", function ValidateBillAndCashAmount() {
+    hidemessage();
+    if (billamount.value >= 0) {
+        if (cashgiven.value >= billamount.value) {
+            const amounttobereturned = cashgiven.value-billamount.value;
+            const calculateChange=amounttobereturned;
+        } else {
+            ShowMessage("The cash provided should be atleast equal to bill amount");
+        }
+    } else {
+        ShowMessage("Invalid Amount");
+    }
+});
+
+function hidemesssage() {
+    message.style.display = "none";
+}
+
+function calculateChange(amounttobereturned){
+    for(let i=0;i<availableNotes.length;i++){
+        const numberOfNotes= Math.trunc(amounttobereturned/ availableNotes[i])
+    };
+    amounttobereturned %= availableNotes[i];
+    noOfNotes[i].innerText=numberOfNotes;
+
+}
+
+function ShowMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
+}
