95c86691c73f2c4a4c532e49b4e1ad4e2c269a45
sivasubramanian11 <Sivaifs4@gmail.com>
2022-08-25 23:06:41 +0530

    Logic done

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const billamount = document.querySelector(".bill-amount");
 const cashgiven = document.querySelector(".cash-given");
 const checkbutton = document.querySelector(".check-button");
 const message = document.querySelector(".error-message");
-const noOfNotes = document.querySelector(".no-Of-Notes");
-
-const availableNotes = [2000, 500, 100, 20, 10, 1];
+const noOfNotes = document.querySelectorAll(".no-Of-Notes");
+const availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 1]
 
 checkbutton.addEventListener("click", function ValidateBillAndCashAmount() {
     hidemessage();
-    if (billamount.value >= 0) {
+    if (billamount.value > 0) {
         if (cashgiven.value >= billamount.value) {
-            const amounttobereturned = cashgiven.value-billamount.value;
-            const calculateChange=amounttobereturned;
+            const amounttobereturned = cashgiven.value - billamount.value;
+            calculatechange(amounttobereturned);
         } else {
             ShowMessage("The cash provided should be atleast equal to bill amount");
         }
@@ -19,20 +18,18 @@ checkbutton.addEventListener("click", function ValidateBillAndCashAmount() {
         ShowMessage("Invalid Amount");
     }
 });
+function calculatechange(amounttobereturned){
+    for(let i=0;i<availableNotes.length;i++){
+        const numberOfNotes= Math.trunc(amounttobereturned / availableNotes[i]
+        );
+        amounttobereturned = amounttobereturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
 
-function hidemesssage() {
+function hidemessage(){
     message.style.display = "none";
 }
-
-function calculateChange(amounttobereturned){
-    for(let i=0;i<availableNotes.length;i++){
-        const numberOfNotes= Math.trunc(amounttobereturned/ availableNotes[i])
-    };
-    amounttobereturned %= availableNotes[i];
-    noOfNotes[i].innerText=numberOfNotes;
-
-}
-
 function ShowMessage(msg) {
     message.style.display = "block";
     message.innerText = msg;
